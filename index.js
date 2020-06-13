899190a0b60bec443ddf3246c4d3887b8b09be9f
nathan <cerisara.nathan@gmail.Com>
2020-06-13 18:33:01 +0200

    aa

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 
+Array.prototype.first = function () {
+    return this[0];
+};
+
 function inpute_key(e){
     if(e && e.keyCode == 13) {
         search();
     }
 }
 
-function search(){
+function nettoyage(){
     //
-    for(c of document.getElementById("liste_mots").children){
-        document.getElementById("liste_mots").removeChild(c);
+    while(document.getElementById("liste_mots").children.length>0){
+        for(c of document.getElementById("liste_mots").children){
+            document.getElementById("liste_mots").removeChild(c);
+        }
     }
     document.getElementById("liste_mots").children=[];
     console.log(document.getElementById("liste_mots").children);
+}
+
+function search(){
+    nettoyage();
     //
     txt=document.getElementById("inpute").value;
     for(w of words){
